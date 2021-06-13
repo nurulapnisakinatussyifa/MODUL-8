5938cd679a2bee0184c0b1fff90c3829e21a6c3d
nurul apni sakinatus syifa <72775881+nurulapnisakinatussyifa@users.noreply.github.com>
2021-06-13 10:47:34 +0700

    Add files via upload

---
+++ TreeNode.java
@@ -0,0 +1,11 @@
+
+package BinaryTree;
+public class TreeNode {
+    char data;
+    TreeNode left;
+    TreeNode right;
+    
+    public TreeNode (char data) {
+        this.data = data;
+    }
+}
